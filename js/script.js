83333fd026a3c79c752eb55eb64d42af2c2aa6a0
YjangWynter <yjangwynter@gmail.com>
2020-01-31 00:32:30 -0500

    got a basic version of the site,  comment tomorrow

--- js/script.js
+++ js/script.js
@@ -3,4 +3,8 @@ var footer = document.getElementById('copyright');
 var year = new Date().getFullYear();
 function copyright() {
     footer.innerHTML = '&copy; ' + year + ' by Yjang Wynter ';
-}  window.addEventListener('load', copyright, false); 
+} 
+
+//I shoudl create a function that whenver the user clicks on an icon, the corresponding details dialog opens and closes
+
+window.addEventListener('load', copyright, false); 
